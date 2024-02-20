132227c14cc332cd9de8bb5cab0bb26223d9476f
Subrata Mondal <subratasubha2@gmail.com>
2024-02-20 17:07:56 +0530

    Rendering List with Dynamic Data via .map

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,10 +1,14 @@
 function ListGroup() {
+  const items = ["Kolkata", "Mumbai", "Bangalore", "Pune"];
   return (
     <ul className="m-8 space-y-8 rounded-lg border-2 border-fuchsia-600 p-8">
-      <li className="border-b-2 p-2 text-xl font-bold">Superman</li>
-      <li className="border-b-2 p-2 text-xl font-bold">Batman</li>
-      <li className="border-b-2 p-2 text-xl font-bold">Ironman</li>
-      <li className="border-b-2 p-2 text-xl font-bold">Watchman</li>
+      {items.map((item) => (
+        <li
+          key={item}
+          className="border-b-2 p-2 text-xl font-bold">
+          {item.toUpperCase()}
+        </li>
+      ))}
     </ul>
   );
 }
