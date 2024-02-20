720b2e537343b39476d55258ae3e8d45f3b4d96e
Subrata Mondal <subratasubha2@gmail.com>
2024-02-20 16:10:30 +0530

    Creating ListGroup with JSX & TailwindCSS

---
+++ react-app/src/components/ListGroup.tsx
@@ -0,0 +1,12 @@
+function ListGroup() {
+  return (
+    <ul className="m-8 space-y-8 rounded-lg border-2 border-fuchsia-600 p-8">
+      <li className="border-b-2 p-2 text-xl font-bold">Superman</li>
+      <li className="border-b-2 p-2 text-xl font-bold">Batman</li>
+      <li className="border-b-2 p-2 text-xl font-bold">Ironman</li>
+      <li className="border-b-2 p-2 text-xl font-bold">Watchman</li>
+    </ul>
+  );
+}
+
+export default ListGroup;
