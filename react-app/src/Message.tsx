9348f9394dd1d1e298fe87658b85373443400747
Subrata Mondal <subratasubha2@gmail.com>
2024-02-20 15:52:00 +0530

    Add TailwindCSS Configuration

--- react-app/src/Message.tsx
+++ react-app/src/Message.tsx
@@ -2,7 +2,7 @@ function Message ()
 {
     const name = "Subrata Mondal"
     if ( name )
-        return <h1>Hello {name}</h1>
-    return <h1>Hello World</h1>
+        return <h1 className="text-red-400">Hello {name}</h1>
+    return <h1 className="text-red-400">Hello World</h1>
 }
 export default Message
