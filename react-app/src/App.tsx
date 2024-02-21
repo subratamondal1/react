57bcf28121d68f344b2e8cf15cbff446a93acd9f
Subrata Mondal <subratasubha2@gmail.com>
2024-02-21 10:30:43 +0530

    Updating states in React e.g.3

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,12 +1,20 @@
 import { useState } from "react";
 function App() {
-  const [pizza, setPizza] = useState({
-    name: "Spicy Pepperoni",
-    toppings: ["Mushroom"],
+  const [cart, setCart] = useState({
+    discount: 0.1,
+    items: [
+      { id: 1, title: "Product 1", quantity: 10 },
+      { id: 2, title: "Product 2", quantity: 5 },
+    ],
   });
   const handleClick = () => {
-    setPizza({ ...pizza, toppings: [...pizza.toppings, "Cheese"] });
-    console.log(pizza);
+    setCart({
+      ...cart,
+      items: cart.items.map((item) =>
+        item.id === 1 ? { ...item, quantity: item.quantity + 1 } : item
+      ),
+    });
+    console.log(cart);
   };
   return (
     <>
