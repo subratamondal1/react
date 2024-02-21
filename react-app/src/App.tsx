1141d32fd4c7e7ffc7cb2356fadbba37697a4e5a
Subrata Mondal <subratasubha2@gmail.com>
2024-02-21 18:25:27 +0530

    Created Form

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,22 +1,6 @@
-import ExpandableText from "./components/ExpandableText";
+import Form from "./components/Form";
 function App() {
-  return (
-    <>
-      <ExpandableText maxCharacters={100}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
-        reiciendis sapiente iure quam eos consectetur, qui veritatis quisquam
-        illo rerum possimus impedit omnis enim a deleniti quod eligendi, vitae
-        officia ipsum expedita adipisci. Vel inventore enim quia voluptate,
-        incidunt amet eligendi quas iusto sapiente, asperiores temporibus
-        blanditiis repellat odit ad fugit sed ab ducimus. Perferendis
-        perspiciatis sunt adipisci aperiam possimus quidem neque eligendi!
-        Dolores consectetur, doloribus illum et accusantium esse quisquam quidem
-        minus sit, corrupti aliquam repellat! Rerum saepe soluta recusandae quae
-        cumque culpa temporibus reprehenderit aut facere sequi, tempora fugit
-        ullam quos minima id odio in voluptates velit enim.
-      </ExpandableText>
-    </>
-  );
+  return <Form></Form>;
 }
 
 export default App;
---
+++ react-app/src/components/Form.tsx
@@ -0,0 +1,39 @@
+function Form() {
+  return (
+    <form
+      action=""
+      className="text-center">
+      <div className="border-fuchsia-100">
+        <label
+          className="block"
+          htmlFor="name">
+          Name
+        </label>
+        <input
+          id="name"
+          type="text"
+          className="border-2 border-fuchsia-200 p-2 mx-4 rounded-lg"
+        />
+      </div>
+      <div className="border-fuchsia-100">
+        <label
+          className="block"
+          htmlFor="age">
+          Age
+        </label>
+        <input
+          id="age"
+          type="number"
+          className="border-2 border-fuchsia-200 p-2 mx-4 rounded-lg"
+        />
+      </div>
+      <button
+        type="submit"
+        className="px-4 py-2 my-2 bg-fuchsia-300 font-bold rounded-full hover:bg-fuchsia-400">
+        Submit
+      </button>
+    </form>
+  );
+}
+
+export default Form;
